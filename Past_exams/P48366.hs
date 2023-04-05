e1d72faf68d5cdbdbd34c86b741878c8055e60ec
pol-fradera <polfradera2@gmail.com>
2023-04-05 14:50:11 +0200

    Update

--- Past_exams/P48366.hs
+++ Past_exams/P48366.hs
@@ -3,19 +3,23 @@ eval1 l = eval1' (words l) []
 
 eval1' :: [String] -> [Int] -> Int
 eval1' [] (y:p) = y
-eval1' l p
-    | x ==  "+" = eval1' (tail l) ((e2+e1):(tail $ tail p))
-    | x ==  "-" = eval1' (tail l) ((e2-e1):(tail $ tail p))
-    | x ==  "*" = eval1' (tail l) ((e2*e1):(tail $ tail p))
-    | x ==  "/" = eval1' (tail l) ((div e2 e1):(tail $ tail p))
-    | otherwise = eval1' (tail l) (z:p)
+eval1' ("+":l) (e1:e2:pila) = eval1' l ((e2+e1):pila)
+eval1' ("-":l) (e1:e2:pila) = eval1' l ((e2-e1):pila)
+eval1' ("*":l) (e1:e2:pila) = eval1' l ((e2*e1):pila)
+eval1' ("/":l) (e1:e2:pila) = eval1' l ((div e2 e1):pila)
+eval1' (x:l) pila = eval1' l (z:pila)
         where 
-            x = head l
             z = read (x):: Int
-            e1 = head p
-            e2 = head $ tail p
-
-
+         
 fsmap :: a -> [a -> a] -> a
 fsmap x [] = x
 fsmap x (f:fs) = fsmap (f x) fs
+
+-- data Racional = (Integer "/" Integer) deriving (Show) deriving (Eq)
+
+-- racional :: Integer -> Integer -> Racional
+-- racional num den = show num ++ "/" ++ show den 
+
+
+-- numerador :: Racional -> Integer
+-- denominador :: Racional -> Integer
