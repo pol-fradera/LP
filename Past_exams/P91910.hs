10076781e097fd1af71319e33b8c2013785baba6
pol-fradera <polfradera2@gmail.com>
2023-04-06 14:18:24 +0200

    Update

--- Past_exams/P91910.hs
+++ Past_exams/P91910.hs
@@ -1,5 +1,10 @@
 multEq :: Int -> Int -> [Int]
 multEq x y = iterate ((*x) . (*y)) 1
 
-selectFirst :: [Int] -> [Int] -> [Int] -> [Int]
-  
+-- selectFirst :: [Int] -> [Int] -> [Int] -> [Int]
+-- selectFirst [] _ _ = []
+-- selectFirst _ [] _ = []
+
+myIterate :: (a -> a) -> a -> [a]
+myIterate f n = scanl (\x _ -> f x)  n [1..]  -- x es el que es el valor intermig 
+                                              -- _ es el valor de la llista [1..]
